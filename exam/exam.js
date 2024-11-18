4822836bda7aa851ed5533343e9191920b420d80
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-11-18 09:21:28 +0900

    lesson08

--- exam/exam.js
+++ exam/exam.js
@@ -1,17 +1,25 @@
 'use strict';
 
 {
+
+  function setWord(){
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    loc = 0;
+  }
+
   const words = [
     'red',
     'blue',
     'pink',
   ];
+
+  const target = document.getElementById('target');
+
   let word;
   let loc = 0;
 
-  const target = document.getElementById('target');
-  word = words[Math.floor(Math.random() * words.length)];
-  target.textContent = word;
+  setWord();
 
   document.addEventListener('keydown', e => {
     if(e.key !== word[loc]){
@@ -22,5 +30,8 @@
     // 2:__d
     // 3:___
     target.textContent = '_'.repeat(loc) + word.substring(loc);
+    if (loc === word.length){
+      setWord();
+    }
   });
 }

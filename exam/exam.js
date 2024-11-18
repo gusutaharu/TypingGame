9797847f40eb1f1f47610a2be5691b9880a70b37
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-11-18 09:40:19 +0900

    lesson011

--- exam/exam.js
+++ exam/exam.js
@@ -18,8 +18,17 @@
 
   let word;
   let loc = 0;
+  let startTime;
+  let isPlaying = false;
 
-  setWord();
+  document.addEventListener('click',()=>{
+    if (isPlaying === true){
+      return;
+    }
+    isPlaying = true;
+    startTime = Date.now();
+    setWord();
+  })
 
   document.addEventListener('keydown', e => {
     if(e.key !== word[loc]){
@@ -32,8 +41,9 @@
     target.textContent = '_'.repeat(loc) + word.substring(loc);
     if (loc === word.length){
       if (words.length === 0){
+        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
         const result = document.getElementById('result');
-        result.textContent = 'Finished!';
+        result.textContent = `Finished! ${elapsedTime} seconds!`;
         return;
       }
       setWord();
